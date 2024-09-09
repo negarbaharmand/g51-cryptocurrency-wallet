f1e1537fbe2c988b94e200cda534b18a8cdeb9b1
Negar Baharmand <negarbaharmand@outlook.com>
2024-09-09 10:13:42 +0200

    initial commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,13 @@
+package se.lexicon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //WalletDao walletDao = new WalletDaoImpl();
+    }
+}
